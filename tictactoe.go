60c72ef308916e6298803427d70f41753d949ae2
Dennis Hein <heindennis@hotmail.de>
2016-04-02 10:15:51 +0200

    removed extra file
    removed Utility.go; did not seem to be justified to have an extra file
    for 2 short helper functions

--- tictactoe.go
+++ tictactoe.go
@@ -103,3 +103,25 @@ func checkGameStatus(g *Game, x, y int) {
 		g.hasWinner = true
 	}
 }
+
+/* Helper functions */
+
+func mapValue(x int) string {
+	switch {
+	case x == 0:
+		return "X"
+	case x == 1:
+		return "O"
+	default:
+		return " "
+	}
+}
+
+func checkFor(value int, items ...int) bool {
+	for _, item := range items {
+		if value == item {
+			return true
+		}
+	}
+	return false
+}
